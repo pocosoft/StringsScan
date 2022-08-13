07db5aa63730d086d29965c71a3e0406724dc899
Akihiro Orikasa <a.orikasa@pocosoft.jp>
2022-08-13 12:30:25 +0900

    Add test files

---
+++ Tests/XcodeTests/SwiftUIProject/SwiftUIProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIProject
+//
+//  Created by Akihiro Orikasa on 2022/08/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
